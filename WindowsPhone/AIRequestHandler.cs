8b34e325bae760472aa992f1ea61e5a850616246
Pedro Filipe <pedro@adeven.com>
2014-01-07 15:48:05 +0100

    clean web request version

--- WindowsPhone/AIRequestHandler.cs
+++ WindowsPhone/AIRequestHandler.cs
@@ -24,11 +24,7 @@ namespace adeven.AdjustIo
         {
             Worker = new BackgroundWorker();
             Worker.WorkerSupportsCancellation = true;
-
-            //choose implementation Native (WebRequest) or Library (Microsoft.Net.Http)
-            //Worker.DoWork += new DoWorkEventHandler(SendPackageWebRequest);
             Worker.DoWork += new DoWorkEventHandler(SendPackageHttpClient);
-            
             Worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(WorkerSendCompleted);
 
             PackageHandler = packageHandler;
@@ -42,7 +38,6 @@ namespace adeven.AdjustIo
             }
         }
 
-        #region HttpClient
         private void SendPackageHttpClient(object sender, DoWorkEventArgs eventArgs)
         {
             SendPackageHttpClientAsync(eventArgs).Wait();
@@ -105,74 +100,7 @@ namespace adeven.AdjustIo
                 eventArgs.Cancel = true;
             }
         }
-        #endregion
-
-        #region WebRequest
-        private void SendPackageWebRequest(object sender, DoWorkEventArgs eventArgs)
-        {
-            SendRequestPackageAsync(eventArgs).Wait();
-        }
-
-        private async Task SendRequestPackageAsync(DoWorkEventArgs eventArgs)
-        {
-            var package = eventArgs.Argument as AIActivityPackage;
-            try
-            {
-                var url = Util.BaseUrl + package.Path;
-                //var url = "http://www.google.com:81";
-                //"http://www.google.com:81"; //timeout test
-                //"invalid site"
-
-                var request = WebRequest.Create(url);
-                request.ContentType = "application/x-www-form-urlencoded";
-                request.Method = "POST";
-                request.Headers["Client-SDK"] = package.ClientSdk;
-                request.Headers["User-Agent"] = package.UserAgent;
-
-                var postStreamTask = request.GetRequestStreamAsync();
-
-                var paramString = Util.GetStringEncodedParameters(package.Parameters);
-
-                // Convert the string into a byte array
-                byte[] byteArray = Encoding.UTF8.GetBytes(paramString);
-
-                var postStream = await postStreamTask;
-                // Write to the request stream
-                postStream.Write(byteArray, 0, paramString.Length);
-                postStream.Dispose();
-
-                using (var webResponse = await request.GetResponseAsync() as HttpWebResponse)
-                {
-                    if (webResponse.StatusCode == HttpStatusCode.OK)
-                    {
-                        AILogger.Info("{0}", package.SuccessMessage());
-                    }
-                    else
-                    {
-                        AILogger.Error("{0}, Status not OK: {1} and response: '{2}'", package.FailureMessage()
-                                                                                , webResponse.StatusCode
-                                                                                , readResponse(webResponse));
-                    }
-                }
-            }
-            catch (WebException we)
-            {
-                using (var response = we.Response as HttpWebResponse)
-                {
-                    AILogger.Error("{0}. WebException with Status {1} and response: '{2}'. Will retry later"
-                                    , package.FailureMessage()
-                                    , response.StatusCode
-                                    , readResponse(response));
-                }
-                eventArgs.Cancel = true;
-            }
-            catch (Exception ex)
-            {
-                AILogger.Error("{0}. Exception: {1}. Will retry later", package.FailureMessage(), ex.Message);
-                eventArgs.Cancel = true;
-            }
-        }
-
+        
         private static string readResponse(HttpWebResponse response)
         {
             using (var streamResponse = response.GetResponseStream())
@@ -181,7 +109,6 @@ namespace adeven.AdjustIo
                 return streamReader.ReadToEnd().Trim();
             }
         }
-        #endregion
 
         private void WorkerSendCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
